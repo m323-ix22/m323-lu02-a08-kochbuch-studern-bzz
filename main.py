a4421d53bf572a80fcc6382d25a4e8a3a73ef8f0
studern-bzz <studern@bzz.ch>
2024-08-28 10:43:36 +0200

    tests passed

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 """Docstring"""
 import json
+
+
 def load_recipe(recipe):
     recipe_dictionary = json.loads(recipe)
     return recipe_dictionary
 
-def adjust_recipe(recipe,number_of_people):
-    if recipe['servings'] == number_of_people:
+def adjust_recipe(recipe, number_of_people):
+    if recipe["servings"] == number_of_people:
         return recipe
     else:
-        multiplicator = number_of_people / recipe['servings']
+        multiplicator = number_of_people / recipe["servings"]
         recipe_new = recipe
         recipe_new["ingredients"]["Spaghetti"] = recipe_new["ingredients"]["Spaghetti"] * multiplicator
         recipe_new["ingredients"]["Tomato Sauce"] = recipe_new["ingredients"]["Tomato Sauce"] * multiplicator
@@ -17,9 +19,8 @@ def adjust_recipe(recipe,number_of_people):
 
     return recipe_new
 
-
-if __name__ == '__main__':
-        recipe_json = '{"title": "Spaghetti Bolognese", "ingredients": {"Spaghetti": 400, "Tomato Sauce": 300, "Minced Meat": 500}, "servings": 4}'
-        new_recipe = (load_recipe(recipe_json))
-        print(new_recipe["ingredients"]["Spaghetti"])
-        adjust_recipe(new_recipe,1)
+if __name__ == "__main__":
+    recipe_json = '{"title": "Spaghetti Bolognese", "ingredients": {"Spaghetti": 400, "Tomato Sauce": 300, "Minced Meat": 500}, "servings": 4}'
+    new_recipe = (load_recipe(recipe_json))
+    print(new_recipe["ingredients"]["Spaghetti"])
+    adjust_recipe(new_recipe, 1)
